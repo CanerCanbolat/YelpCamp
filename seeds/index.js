0ac2ece97e2ecc5469982981f2a91696f5cbf9b2
Caner Canbolat <107541646+CanerCanbolat@users.noreply.github.com>
2024-03-29 13:17:27 +0300

    Add files via upload

---
+++ seeds/index.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const cities = require("./cities");
+const descriptors = require("./descriptors");
+const places = require("./places");
+const Campground = require("../models/campground");
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
+  console.log("Database connected");
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const camp = new Campground({
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+    });
+    await camp.save();
+  }
+};
+
+seedDB().then(() => {
+  mongoose.connection.close();
+});
