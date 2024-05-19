c0cb5c1af297ca03458655cc8731145639da0ed7
Caner Canbolat <canercanbo@gmail.com>
2024-05-19 18:24:34 +0300

    cloud data base bağlatı problemleri giderildi. Ancak bootstrap validation işlemleri düzenlenecek.

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 const express = require("express");
-const app = express();
+const bodyParser = require("body-parser");
+//*TODO Bu kod main fonksiyonundan sonra eklenmesi gerekebilir.
+
 const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
@@ -14,29 +16,73 @@ const methodOverride = require("method-override");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+// const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const userRoutes = require("./routes/user.js");
 const campgroundsRoutes = require("./routes/campgrounds.js");
 const reviewsRoutes = require("./routes/reviews.js");
 
+const dbUrl = process.env.DB_URL || "mongodb://127.0.0.1:27017/yelp-camp";
+const MongoDBStore = require("connect-mongo");
+
+// const client = new MongoClient(dbUrl, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   },
+// });
+
 main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
+  await mongoose.connect(dbUrl);
   console.log("Database connected");
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
+
+const app = express();
 
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+//*Bunlar dursun burada yaa
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  mongoSanitize({
+    replaceWith: "_",
+  })
+);
+
+const secret = "thisshouldbeabettersecret!";
+
+// const option = {
+//   url: dbUrl,
+//   secret,
+//   touchAfter: 24 * 3600,
+// };
+
+// const store = MongoDBStore.create({ ...option });
+// console.log(store);
+const store = MongoDBStore.create({
+  mongoUrl: dbUrl,
+  touchAfter: 24 * 60 * 60,
+  crypto: {
+    secret: "thisshouldbeabettersecret!",
+  },
+});
 
 const sessionConfig = {
-  secret: "thisshouldbeabettersecret!",
+  store,
+  secret,
   resave: false,
   saveUninitialized: true,
   cookie: {
@@ -47,6 +93,55 @@ const sessionConfig = {
 };
 app.use(session(sessionConfig));
 app.use(flash());
+app.use(helmet());
+
+const scriptSrcUrls = [
+  "https://stackpath.bootstrapcdn.com/",
+  "https://api.tiles.mapbox.com/",
+  "https://api.mapbox.com/",
+  "https://kit.fontawesome.com/",
+  "https://cdnjs.cloudflare.com/",
+];
+//* All style Url you must add in styleSrcURLS
+const styleSrcUrls = [
+  "https://kit-free.fontawesome.com/",
+  "https://stackpath.bootstrapcdn.com/",
+  "https://api.mapbox.com/",
+  "https://api.tiles.mapbox.com/",
+  "https://fonts.googleapis.com/",
+  "https://use.fontawesome.com/",
+  "https://cdn.jsdelivr.net/",
+];
+//*All apies Url you must add in styleSrcURLS
+const connectSrcUrls = [
+  "https://api.mapbox.com/",
+  "https://a.tiles.mapbox.com/",
+  "https://b.tiles.mapbox.com/",
+  "https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["self"],
+        connectSrc: ["'self'", ...connectSrcUrls],
+        scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+        styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+        workerSrc: ["'self'", "blob:"],
+        objectSrc: [],
+        imgSrc: [
+          "'self'",
+          "blob:",
+          "data:",
+          "https://res.cloudinary.com/djma7yuas/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
+          "https://images.unsplash.com/",
+        ],
+        fontSrc: ["'self'", ...fontSrcUrls],
+      },
+    },
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -79,6 +174,8 @@ app.use((err, req, res, next) => {
   if (!err.message) err.message = "Oh No, Something went wrong!";
   res.status(statusCode).render("error", { err });
 });
+
+const port = dbUrl;
 
 app.listen(3000, () => {
   console.log("Serving on port 3000!");
